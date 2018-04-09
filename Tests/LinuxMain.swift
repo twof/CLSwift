913154b5da10d6b90068a7f6a5b5cce60478f4a9
Félix Fischer <felix91gr@users.noreply.github.com>
2018-04-09 17:58:39 -0300

    Added missing `CLSwiftTests.allTests` member
    * Added through an extension on `LinuxMain.swift`.
    * Tested already under Linux.
    * At the time of this writing, all test pass on Linux (good job everyone!).

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,6 +1,16 @@
 import XCTest
 @testable import CLSwiftTests
 
+extension CLSwiftTests {
+    static var allTests: [(String, (CLSwiftTests) -> () throws -> Void)] = [
+        ("testEmpty", testEmpty),
+        ("testLowerThanMinimumArgumentsInputLength", testLowerThanMinimumArgumentsInputLength),
+        ("testGreaterThanMaximumArgumentsInputLength", testGreaterThanMaximumArgumentsInputLength),
+        ("testWithAssociatedArg", testWithAssociatedArg),
+        ("testFailureWithFlags", testFailureWithFlags),
+    ]
+}
+
 XCTMain([
     testCase(CLSwiftTests.allTests),
 ])
